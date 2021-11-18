b320ab4696b736f884a8be55ddcf0f22fba9d5db
dinar <dinarmkbg@gmail.com>
2021-11-18 12:49:20 +0300

    refactor formats

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,13 +1,10 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
 
-export default (getDiff, format) => {
-  if (format === 'plain') {
-    return plain(getDiff);
-  }
-  if (format === 'json') {
-    return JSON.stringify(getDiff);
-  }
-
-  return stylish(getDiff);
+const formats = {
+  stylish,
+  plain,
+  json: JSON.stringify,
 };
+
+export default (getDiff, format) => formats[format](getDiff);
